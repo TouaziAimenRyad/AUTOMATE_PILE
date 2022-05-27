964f564a84f7870fa7f0cd7096dd4d72d8523d45
bacha <rayanebacha80@gmail.com>
2022-05-27 17:39:33 +0200

    main maj

--- main.ml
+++ main.ml
@@ -7,14 +7,16 @@ let lexbuf = Lexing.from_channel stdin
 
 let ast = Parser.s Lexer.next_token lexbuf 
 
-let main () =
+let usage()=
   print_string "--test_mot to check if a word is accepted by the automate \n";
   print_string "--verifier_automate to check if the automate is correct  \n";
-
+;;
+let main () =
+  
   match Sys.argv with 
-    |[|_;"--test_mot";word|]-> Ast.check_mot ast word
+    |[|_;"--test_mot";word|]-> (try Ast.check_mot ast word with |Ast.Word_accepted->print_string "the word is accepted by the automate\n" |Ast.Word_not_accepted s ->print_string ("the word is not accepted by the automate because :"^s^"\n"))
     |[|_;"--verifier_automate"|]-> Ast.verfier_automate ast
-    |_-> print_string " redo ";
+    |_-> usage();
 ;;
 (* lancement de ce main *)
 
